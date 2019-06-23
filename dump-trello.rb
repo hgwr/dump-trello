b89a58e1f0756c26ac294d22dc9dab655b639c64
Shigeru Hagiwara <hgwrsgr@gmail.com>
2019-06-23 09:44:22 +0900

    *** empty log message ***

--- dump-trello.rb
+++ dump-trello.rb
@@ -19,39 +19,45 @@ end
 board = Trello::Board.find(board_id)
 
 outputs = []
-cards = board.cards
-cards.each do |card|
-  STDERR.print("#{card.short_url} ")
-  sleep 1
+lists = board.lists
+lists.each do |list|
+  list_data = {
+    id: list.id,
+    name: list.name,
+    cards: []
+  }
+  
+  cards = list.cards
+  cards.each do |card|
+    comment_data = []
+    comments_json = card.client.get("/cards/#{card.id}/actions", filter: "commentCard")
+    comments = JSON.parse(comments_json)
+    comments.each do |comment|
+      print(".")
+      comment_data.push({
+                          source: 'Trello',
+                          original_id: comment["id"],
+                          original_parent_id: (comment["data"]["list"]["id"] rescue nil),
+                          content: comment["data"]["text"],
+                          create_by: (comment["memberCreator"]["fullName"] rescue nil),
+                          original_created: Time.parse(comment["date"])
+                        })
+    end
 
-  comment_data = []
-  comments_json = card.client.get("/cards/#{card.id}/actions", filter: "commentCard")
-  comments = JSON.parse(comments_json)
-  comments.each do |comment|
-    print(".")
-    comment_data.push({
-                        source: 'Trello',
-                        original_id: comment["id"],
-                        original_parent_id: (comment["data"]["list"]["id"] rescue nil),
-                        content: comment["data"]["text"],
-                        create_by: (comment["memberCreator"]["fullName"] rescue nil),
-                        original_created: Time.parse(comment["date"])
-                      })
+    card_data = {
+      original_id: card.id,
+      original_parent_id: card.list_id,
+      original_key_string: card.short_url,
+      subject: card.name,
+      content: card.desc,
+      create_by: card.members.map(&:full_name).join(', '),
+      original_updated: card.last_activity_date,
+      comments: comment_data
+    }
+    list_data[:cards].push(card_data)
   end
 
-  card_data = {
-    original_id: card.id,
-    original_parent_id: card.list_id,
-    original_key_string: card.short_url,
-    subject: card.name,
-    content: card.desc,
-    create_by: card.members.map(&:full_name).join(', '),
-    original_updated: card.last_activity_date,
-    comments: comment_data
-  }
-  outputs.push(card_data)
-  
-  STDERR.puts("")
+  outputs.push(list_data)
 end
 
 puts JSON.pretty_generate(outputs)
